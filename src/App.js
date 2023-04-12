c91a1a3ca9465c295abf26f39bcffcc9bc9cbd3f
dbrudsodlf <contiki0527@naver.com>
2023-04-12 23:16:43 +0900

    react-todolist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { editableInputTypes } from '@testing-library/user-event/dist/utils';
 
 function App() {
+  const [toDo,setToDo]=useState("");
+  const [toDos,setToDos]=useState([]);
+  const onChange=(event)=>setToDo(event.target.value);
+  const onSubmit=(event)=>{
+    event.preventDefault();
+    if(toDo===""){
+      return;
+    }
+    setToDos((currnetArray)=>[toDo,...currnetArray]);
+    setToDo("");
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type='text'
+          placeholder='Write your plan...'        
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr/>
+      <ul>
+        {toDos.map((item,index)=>
+        (<li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
