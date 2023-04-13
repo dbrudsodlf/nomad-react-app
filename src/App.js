da872a004ec4bc0c377d3058500507724c598e23
dbrudsodlf <contiki0527@naver.com>
2023-04-13 23:24:13 +0900

    react-router

--- src/App.js
+++ src/App.js
@@ -1,39 +1,5 @@
-import { useState } from 'react';
-import './App.css';
-import { editableInputTypes } from '@testing-library/user-event/dist/utils';
-
 function App() {
-  const [toDo,setToDo]=useState("");
-  const [toDos,setToDos]=useState([]);
-  const onChange=(event)=>setToDo(event.target.value);
-  const onSubmit=(event)=>{
-    event.preventDefault();
-    if(toDo===""){
-      return;
-    }
-    setToDos((currnetArray)=>[toDo,...currnetArray]);
-    setToDo("");
-  };
-  return (
-    <div>
-      <h1>My ToDos  ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type='text'
-          placeholder='Write your plan...'        
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr/>
-      <ul>
-        {toDos.map((item,index)=>
-        (<li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return null;
 }
 
 export default App;
