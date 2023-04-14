436fd1fdfc56cdc207c8a66b950d09cf8e9d7a8e
dbrudsodlf <contiki0527@naver.com>
2023-04-14 22:44:22 +0900

    react-url

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import Movie from './components/Movie';
+import Movie from '../components/Movie';
 
 function Home(){
     const [loading,setLoading] = useState(true);
@@ -22,6 +22,7 @@ function Home(){
       <div>{movies.map((movie)=>(
         <Movie 
         key={movie.id}
+        id={movie.id}
         coverImg={movie.medium_cover_image}
         title={movie.title}
         summary={movie.summary}
