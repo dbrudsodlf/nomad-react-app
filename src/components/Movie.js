7c2846f4bc1670c021b384d7c2f85be624e98aae
dbrudsodlf <contiki0527@naver.com>
2023-04-16 23:59:06 +0900

    css

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,15 @@
 import { PropTypes } from "prop-types";
 import {Link} from "react-router-dom"
-
-function Movie({id,coverImg, title, summary, genres }) {
+import './Movie.css'
+function Movie({id,year,coverImg, title, summary, genres }) {
     return (
-        <div>
-            <img src={coverImg} alt="cover"/>
-            <h2>
+        <div className="movie">
+            <img className="movie_img" src={coverImg} alt="cover"/>
+            <h2 className="movie_title">
                 <Link to={`/movie/${id}`}>{title}</Link></h2>
+            <h3 className="movie_year">{year}</h3>
             <p>{summary.length>235?`${summary.slice(0,235)}...`:summary}</p>
-            <ul>
+            <ul className="movie_genres">
                 {genres.map((g) => (
                     <li key={g}>{g}</li>
                 ))}
