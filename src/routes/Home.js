b13cc196d5c225d7d17c888da99290dafa354834
dbrudsodlf <contiki0527@naver.com>
2023-04-17 23:21:51 +0900

    movie_web_css_done

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,39 +2,40 @@ import { useEffect, useState } from 'react';
 import Movie from '../components/Movie';
 import './Home.css';
 
-function Home(){
-    const [loading,setLoading] = useState(true);
-  const[movies,setMovies]=useState([]);
-  const getMovies=async()=>{
-    const json = await(
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
       await fetch(
-      'https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year'
+        'https://yts.mx/api/v2/list_movies.json?summary!=null&minimum_rating=8.8&sort_by=year'
       )).json();
-      setMovies(json.data.movies);
-      setLoading(false);
+    setMovies(json.data.movies);
+    setLoading(false);
   };
-  useEffect(()=>{
-   getMovies();
-  },[]);
+  useEffect(() => {
+    getMovies();
+  }, []);
 
   return (
     <div className='container'>
-      {loading ? 
-      <div className='loader'>
-        <span>Loading...</span>
-      </div>:
-      <div className='movies'>{movies.map((movie)=>(
-        <Movie 
-        key={movie.id}
-        id={movie.id}
-        year={movie.year}
-        coverImg={movie.medium_cover_image}
-        title={movie.title}
-        summary={movie.summary}
-        genres={movie.genres}
-        />
+      {loading ?
+        <div className='loader'>
+          <span>Loading...</span>
+        </div> :
+        <div className='movies'>{movies.map((movie) => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            year={movie.year}
+            coverImg={movie.medium_cover_image}
+            title={movie.title}
+            summary={movie.summary}
+            genres={movie.genres}
+            rating={movie.rating}
+          />
         )
-       
+
         )}</div>}
     </div>
 
